22116496badb028e75c417bf731d65a696e41675
AphisitMA <aphisit.manivong@gmail.com>
2025-02-08 19:21:27 +0700

    Add Exercise

---
+++ Exercise4_Aphisit_M.py
@@ -0,0 +1,9 @@
+FE = 4
+GB = 3.5
+ICS = 4
+CP = 4
+print("---Your Score---")
+print("Foundation English = ", FE)
+print("General Business = ", GB)
+print("Introduction to Computer systems = ", ICS)
+print("Computer Programming = ", CP)
